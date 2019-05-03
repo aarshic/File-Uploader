188b96428d4b13d1643b7d4b69b8fd41c80cad45
aarshic <aarshi.1998@gmail.com>
2019-05-04 01:57:10 +0530

    Added few new features

--- login.js
+++ login.js
@@ -5,54 +5,7 @@ function validate(){
     var password = document.getElementById("password").value;
     if ( username == "Formget" && password == "formget#123"){
         alert ("Login successfully");
-        window.location = "http://localhost:8080/"; // Redirecting to other page.
-        {
-            res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-            res.write('<input type="file" name="filetoupload"><br>');
-            res.write('<input type="submit">');
-            res.write('</form>');
-            // if(req.url == '/fileupload') {
-            //     var form = new formidable.IncomingForm();
-            //     form.parse(req, function (err, fields, files) {
-            //         res.write('File uploaded');
-
-
-            //         {
-            //             if (req.url == '/fileupload') {
-            //                 var form = new formidable.IncomingForm();
-            //                 form.parse(req, function (err, fields, files) {
-            //                     var oldpath = files.filetoupload.path;
-            //                     var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-            //                     fs.rename(oldpath, newpath, function (err) {
-            //                         if (err) throw err;
-            //                             res.write('File uploaded and moved!');
-            //                         res.end();
-            //                 });
-            //             });
-            //             } 
-            //             else {
-            //                 res.writeHead(200, {'Content-Type': 'text/html'});
-            //                 res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-            //                 res.write('<input type="file" name="filetoupload"><br>');
-            //                 res.write('<input type="submit">');
-            //                 res.write('</form>');
-            //                 return res.end();
-            //             }
-            //         }
-
-            //         res.end();
-            //     });
-            // } 
-            // else {
-            //     res.writeHead(200, {'Content-Type': 'text/html'});
-            //     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-            //     res.write('<input type="file" name="filetoupload"><br>');
-            //     res.write('<input type="submit">');
-            //     res.write('</form>');
-            //     return res.end();
-            // }
-            return res.end();
-        }
+        window.location = "index.html"; // Redirecting to other page.
         return false;
     }
     else{
--- save.js
+++ save.js
@@ -1,25 +1,31 @@
-// var http = require('http');
-// var formidable = require('formidable');
-// var fs = require('fs');
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
 
-// http.createServer(function (req, res) {
-//   if (req.url == '/fileupload') {
-//     var form = new formidable.IncomingForm();
-//     form.parse(req, function (err, fields, files) {
-//       var oldpath = files.filetoupload.path;
-//       var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-//       fs.rename(oldpath, newpath, function (err) {
-//         if (err) throw err;
-//         res.write('File uploaded and moved!');
-//         res.end();
-//       });
-//  });
-//   } else {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-//     res.write('<input type="file" name="filetoupload"><br>');
-//     res.write('<input type="submit">');
-//     res.write('</form>');
-//     return res.end();
-//   }
-// }).listen(8080);
+http.createServer(function (req, res) {
+    var fileuploaded1 = document.getElementById("fileuploaded1").value;
+    var fileuploaded2 = document.getElementById("fileuploaded2").value;
+    var fileuploaded3 = document.getElementById("fileuploaded3").value;
+    var fileuploaded4 = document.getElementById("fileuploaded4").value;
+    var fileuploaded5 = document.getElementById("fileuploaded5").value;
+    if (req.url == '/fileupload') {
+        var form = new formidable.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+            var oldpath = files.filetoupload.path;
+            var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+            fs.rename(oldpath, newpath, function (err) {
+                if (err) throw err;
+                res.write('File uploaded and moved!');
+                res.end();
+            });
+        });
+    } 
+    // else {
+    //     res.writeHead(200, {'Content-Type': 'text/html'});
+    //     res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+    //     res.write('<input type="file" name="filetoupload"><br>');
+    //     res.write('<input type="submit">');
+    //     res.write('</form>');
+    //     return res.end();
+    // }
+}).listen(8080);
