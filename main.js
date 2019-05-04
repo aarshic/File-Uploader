cf181910d9cb4caf898db27af258de902ed4ee43
aarshic <aarshi.1998@gmail.com>
2019-05-04 16:43:41 +0530

    Adding data to the database

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
-var http = require('http');
+const express=require("express");
+var router = express.Router();
+// const mongoose=require("mongoose");
+var mongo = require('mongodb');
+var assert = require('assert');
 
-var server = http.createServer(function(req, res) {
-res.writeHead(200);
-res.end('Hi everybody!');
+var url = 'mongodb://localhost:27017/test';
+
+router.get('/', function(req, res, next){
+    res.render('index');
 });
-server.listen(8080);
+
+router.get('/get-data', function(req, res, next){
+    var resultArray = [];
+    mongo.connect(url, function(err, db){
+        assert.equal(null, err);
+        var cursor = db.collection('user-data').find();
+        cursor.forEach(function(doc, err){
+            assert.equal(null, err);
+            if(assert.equal(u,username) && assert.equal(p,password)){
+                res.render('index.html');    
+            }
+        }, function(){
+            db.close();
+            res.render('login.html', {login: resultArray});
+        });
+    });
+});
+
+router.post('/insert', function(req, res, next){
+    var login = {
+        username: req.body.username,
+        password: req.body.password
+    };
+    mongo.connect(url, function(err, db){
+        assert.equal(null,err);
+        db.collection('user-data').insertOne(login, function(err,result){
+            console.log('Item inserted')
+        });
+    });
+    res.redirect('/');
+});
+
+// router.get('/update', function(req, res, next){
+// });
+
+// router.post('/delete', function(req, res, next){
+// });
+
+module.exports = router;
